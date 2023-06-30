04c9791ad11193cfc0c8d257867db9da3db2a67b
Emprario <pierre.cameleri@free.fr>
2023-06-30 19:09:13 +0200

    base of scripts to detect boot partitions

--- mkinitramfs.sh
+++ mkinitramfs.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 BUILD_PATH=$(pwd)
 INITRAMFS_PATH=$BUILD_PATH/initramfs
 INIT_SCRIPT=$BUILD_PATH/init
@@ -22,23 +24,10 @@ cp $INIT_SCRIPT .
 curl https://www.busybox.net/downloads/binaries/1.21.1/busybox-x86_64 -Lo ./bin/busybox
 chmod +x ./bin/busybox
 cd bin
-ln -s busybox cat
-ln -s busybox cd
-ln -s busybox chvt
-ln -s busybox clear
-ln -s busybox echo
-ln -s busybox ln
-ln -s busybox ls
-ln -s busybox mkdir
-ln -s busybox mount
-ln -s busybox modprobe
-ln -s busybox insmod 
-ln -s busybox rm
-ln -s busybox rmdir
-ln -s busybox sh
-ln -s busybox touch
-ln -s busybox tty
-ln -s busybox vi
+for cmd in "[" "[[" acpid add-shell addgroup adduser adjtimex arp arping ash awk base64 basename beep blkid blockdev bootchartd brctl bunzip2 bzcat bzip2 cal cat catv chat chattr chgrp chmod chown chpasswd chpst chroot chrt chvt cksum clear cmp comm conspy cp cpio crond crontab cryptpw cttyhack cut date dc dd deallocvt delgroup deluser depmod devmem df dhcprelay diff dirname dmesg dnsd dnsdomainname dos2unix du dumpkmap dumpleases echo ed egrep eject env envdir envuidgid ether-wake expand expr fakeidentd false fbset fbsplash fdflush fdformat fdisk fgconsole fgrep find findfs flock fold free freeramdisk fsck fsck.minix fsync ftpd ftpget ftpput fuser getopt getty grep groups gunzip gzip halt hd hdparm head hexdump hostid hostname httpd hush hwclock id ifconfig ifdown ifenslave ifplugd ifup inetd init insmod install ionice iostat ip ipaddr ipcalc ipcrm ipcs iplink iproute iprule iptunnel kbd_mode kill killall killall5 klogd last less linux32 linux64 linuxrc ln loadfont loadkmap logger login logname logread losetup lpd lpq lpr ls lsattr lsmod lsof lspci lsusb lzcat lzma lzop lzopcat makedevs makemime man md5sum mdev mesg microcom mkdir mkdosfs mke2fs mkfifo mkfs.ext2 mkfs.minix mkfs.vfat mknod mkpasswd mkswap mktemp modinfo modprobe more mount mountpoint mpstat mt mv nameif nanddump nandwrite nbd-client nc netstat nice nmeter nohup nslookup ntpd od openvt passwd patch pgrep pidof ping ping6 pipe_progress pivot_root pkill pmap popmaildir poweroff powertop printenv printf ps pscan pstree pwd pwdx raidautorun rdate rdev readahead readlink readprofile realpath reboot reformime remove-shell renice reset resize rev rm rmdir rmmod route rpm rpm2cpio rtcwake run-parts runlevel runsv runsvdir rx script scriptreplay sed sendmail seq setarch setconsole setfont setkeycodes setlogcons setserial setsid setuidgid sh sha1sum sha256sum sha3sum sha512sum showkey slattach sleep smemcap softlimit sort split start-stop-daemon stat strings stty su sulogin sum sv svlogd swapoff swapon switch_root sync sysctl syslogd tac tail tar tcpsvd tee telnet telnetd test tftp tftpd time timeout top touch tr traceroute traceroute6 true tty ttysize tunctl udhcpc udhcpd udpsvd umount uname unexpand uniq unix2dos unlzma unlzop unxz unzip uptime users usleep uudecode uuencode vconfig vi vlock volname wall watch watchdog wc wget which who whoami whois xargs xz xzcat yes zcat zcip
+do
+    ln -s busybox $cmd
+done
 cd ..
 
 # extract modules to /lib/modules
@@ -59,6 +48,11 @@ cp -r /lib/firmware/amdgpu ./lib/firmware
 
 # add mod.cfg to the initramfs
 #cp  $MOD_D ./etc/mod.cfg 
+
+# add all scripts in scripts.d into /lib/scripts
+mkdir ./lib/scripts
+cp $BUILD_PATH/scripts.d/* ./lib/scripts/
+
 
 # compile cpio archive
 rm ../$CPIO_ARCHIVE
---
+++ scripts.d/scan_boot.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+#### Find bootable drives ####
+echo "Finding bootable drives ..."
+
+IFS='
+' read -a DRIVES <<< $(lsblk -ro name,partlabel | grep "Boot")
+
+# Separate Drives lines to only keep drive name
+DRIVES_PATH=()
+for drive in "${DRIVES[@]}";
+do
+  IFS=' ' read -a d <<< $drive
+  DRIVES_PATH="${DRIVES_PATH} ${d[0]}"
+done
+echo "Boot partitions found: ${#DRIVES_PATH[*]}"
+echo ${DRIVES_PATH[*]}
+
+#### Mount and search for kernels ####
+echo "Mounting and searching for kernels ..."
+
+OPTIONS=()
+for drive in "${DRIVES_PATH[@]}";
+do 
+  drive=$(echo $drive | sed 's/ //g')
+  mount --mkdir /dev/$drive /mnt/$drive
+  cd /mnt/$drive  
+  for folder in "/mnt/$drive"/*;
+  do
+    echo "Scanning: $folder"
+    if   [ -f $folder/bzImage ] \
+      && [ -f $folder/kernel.flags ] \
+      && [ -f $folder/initrd ]; then 
+      OPTIONS="${OPTIONS} '$folder'"
+    fi
+  done
+  cd /
+  umount /dev/$drive
+done
+echo "Options detected: ${#OPTIONS[*]}"
+echo ${OPTIONS[*]}
+return OPTIONS
+
